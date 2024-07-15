aaa71e0aaaacd472e50f6ecac3e8e06e5884e969
Markus Hauck <markus1189@gmail.com>
2024-07-15 08:52:10 +0200

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+
+        hsPkgs = pkgs.haskellPackages;
+
+        appDrv = pkgs.haskell.lib.justStaticExecutables
+          (hsPkgs.callCabal2nix "hlox" ./. { });
+
+        devEnv = hsPkgs.developPackage {
+          returnShellEnv = true;
+          root = ./.;
+          modifier = with pkgs.haskell.lib;
+            drv:
+            dontHaddock (disableOptimization (disableLibraryProfiling drv));
+        };
+      in rec {
+        apps.hlox = {
+          type = "app";
+          program = "${pkgs.haskell.lib.justStaticExecutables appDrv}/bin/hlox";
+        };
+        defaultApp = apps.hlox;
+
+        packages.hlox = appDrv;
+        defaultPackage = packages.hlox;
+
+        devShell = pkgs.mkShell {
+          inputsFrom = [ devEnv ];
+          buildInputs = with hsPkgs; [ haskell-language-server implicit-hie ];
+        };
+      });
+}
